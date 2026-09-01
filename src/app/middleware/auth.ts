a14bba2c02c4d0b4b4319b77471d60ca5806ae14
abusaleh233 <abusaleh233@gmail.com>
2026-09-02 00:01:02 +0600

    auth middleware

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,56 @@
+import type { NextFunction, Request, Response } from "express";
+import config from "../config/index.js";
+import prisma from "../lib/prisma.js";
+import AppError from "../utils/AppError.js";
+import { verifyToken } from "../utils/jwt.js";
+
+declare global {
+	namespace Express {
+		interface Request {
+			user?: {
+				id: string;
+				email: string;
+				role: string;
+			};
+		}
+	}
+}
+
+const auth = (...requiredRoles: string[]) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const token = req.headers.authorization?.split(" ")[1];
+
+			if (!token) {
+				throw new AppError(401, "You are not authorized! Token missing.");
+			}
+
+			// Verify Token
+			const decoded = verifyToken(
+				token,
+				config.jwt_access_secret
+			) as { id: string; email: string; role: string };
+
+			// Check if User Exists in Database
+			const user = await prisma.user.findUnique({
+				where: { id: decoded.id },
+			});
+
+			if (!user) {
+				throw new AppError(404, "User associated with this token was not found!");
+			}
+
+			// Check Role authorization
+			if (requiredRoles.length && !requiredRoles.includes(decoded.role)) {
+				throw new AppError(403, "You do not have permission to access this route!");
+			}
+
+			req.user = decoded;
+			next();
+		} catch (error) {
+			next(error);
+		}
+	};
+};
+
+export default auth;
